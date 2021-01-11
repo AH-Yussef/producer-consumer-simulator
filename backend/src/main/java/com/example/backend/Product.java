3dedfd87cb01212fd4ae070127b943a552a2742a
MohannadSoliman <61092652+MohannadSoliman@users.noreply.github.com>
2021-01-11 12:39:31 +0200

    adding reset and color uniqueness

--- backend/src/main/java/com/example/backend/Product.java
+++ backend/src/main/java/com/example/backend/Product.java
@@ -3,10 +3,14 @@ package com.example.backend;
 import java.util.Random;
 
 public class Product {
+    Simulator simulator = Simulator.getInstance();
     private String color = new String();
     public Product(){
+        //making sure that the colors are unique
+        do{
         this.color = String.valueOf(1+new Random().nextInt(254)) + "," + String.valueOf(1+new Random().nextInt(254)) + "," 
         + String.valueOf(1+new Random().nextInt(254));
+        }while(simulator.colorUsed(this.color));
     }
     public String getColor(){
         return color;
