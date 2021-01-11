130508c5a74917c5ca75262038259ba4e0ee8c45
MohannadSoliman <61092652+MohannadSoliman@users.noreply.github.com>
2021-01-11 15:34:18 +0200

    color uniqueness

--- backend/src/main/java/com/example/backend/Product.java
+++ backend/src/main/java/com/example/backend/Product.java
@@ -6,11 +6,14 @@ public class Product {
     Simulator simulator = Simulator.getInstance();
     private String color = new String();
     public Product(){
-        //making sure that the colors are unique
+        //if color already exists we will keep generating colors until a unique one
         do{
         this.color = String.valueOf(1+new Random().nextInt(254)) + "," + String.valueOf(1+new Random().nextInt(254)) + "," 
         + String.valueOf(1+new Random().nextInt(254));
         }while(simulator.colorUsed(this.color));
+        
+        //if a unique color is found then it is added to the set
+        simulator.addColor(this.color);
     }
     public String getColor(){
         return color;
